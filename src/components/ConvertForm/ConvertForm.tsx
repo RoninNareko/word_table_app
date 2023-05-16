ad8dd3801c1bc52a29bfa413278d7bc1c843771f
Arpine <a.aleksanyan@diagnocat.com>
2023-05-16 18:37:59 +0400

    get data by rage

--- src/components/ConvertForm/ConvertForm.tsx
+++ src/components/ConvertForm/ConvertForm.tsx
@@ -5,7 +5,9 @@ import * as XLSX from "xlsx";
 
 const Converter = () => {
   const [variables, setVariables] = useState<any[]>([]);
+  const [properties, setProperties] = useState<any[]>([]);
   console.log("variables", variables);
+  console.log("properties", properties);
   const doxcFileReader = async (e: any) => {
     e.preventDefault();
     const reader = new FileReader();
@@ -27,22 +29,23 @@ const Converter = () => {
     reader.readAsBinaryString(e.target.files[0]);
   };
 
-  const sheetFileReader = (e: any) => {
-    const file = e.target.files[0];
-    const reader = new FileReader();
-    reader.onload = function (e) {
-      //@ts-ignore
-      const table = XLSX.read(e.target.result);
-      const sheet = table.Sheets[table.SheetNames[2]];
-      var range = XLSX.utils.decode_range("A6:C104");
-      const variables = {
-        list: 1,
-        stolbec: 6,
-        stroka: 10,
-        stolbec2: 104,
-      };
-      var dataRange = [];
-      /* Iterate through each element in the structure */
+  const getRangeData = async (variable: any, sheet: any) => {
+    const variableType = variable.stroka && !variable.strokaTwo;
+    let range: any = null;
+    const dataRange: any[] = [];
+
+    if (variableType) {
+      range = XLSX.utils.decode_range(
+        `A${variable.stolbec}:C${variable.stroka}`
+      );
+    }
+    if (!variableType) {
+      range = XLSX.utils.decode_range(
+        `A${variable.stolbec}:C${variable.strokaTwo}`
+      );
+    }
+
+    return new Promise((resolve, reject) => {
       for (let R = range.s.r; R <= range.e.r; ++R) {
         for (let C = range.s.c; C <= range.e.c; ++C) {
           let cell_address = { c: C, r: R };
@@ -53,9 +56,46 @@ const Converter = () => {
           }
         }
       }
-      console.log(dataRange);
-      console.log(sheet, range);
+      // console.log(dataRange, typeof dataRange);
+      setProperties((prevState) => {
+        return [...prevState, dataRange];
+      });
+      resolve(true);
+    });
+  };
+
+  const sheetFileReader = (e: any) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      //@ts-ignore
+      const table = XLSX.read(e.target.result);
+      const sheet = table.Sheets[table.SheetNames[2]];
+
+      /* Iterate through each element in the structure */
+      variables?.forEach(async (variable) => {
+        await getRangeData(variable, sheet);
+      });
+      // variables?.forEach((variable) => {
+      //   console.log("kkkkkkkkkkkkkkk", `A${6}:C${104}`);
+      // const range = XLSX.utils.decode_range(
+      //   `A${6}:C${104}`
+      // );
+      // /* Iterate through each element in the structure */
+      // for (let R = range.s.r; R <= range.e.r; ++R) {
+      //   for (let C = range.s.c; C <= range.e.c; ++C) {
+      //     let cell_address = { c: C, r: R };
+      //     let data = XLSX.utils.encode_cell(cell_address);
+      //     //@ts-ignore
+      //     if (sheet[data]) {
+      //       dataRange.push(sheet[data]);
+      //     }
+      //   }
+      // }
+      // console.log(sheet, range);
       /* DO SOMETHING WITH workbook HERE */
+      // });
+      // console.log("dataRange", dataRange);
     };
     reader.readAsArrayBuffer(file);
   };
